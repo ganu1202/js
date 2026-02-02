0eb15fa964d8c90062947f8d20b0f5c88da1bf2f
ganu1202 <ganeshnidabane06@gmail.com>
2026-02-02 15:12:26 +0530

    conversions of datatypes

---
+++ js_basics/02_dataTypes.js
@@ -0,0 +1,56 @@
+"use strict";// treat all JS code as newer version
+// alert("hello")// we are using nodejs, not browser
+
+console.log(3+3);
+//code redabality should be high
+
+
+// ECMAScript (ES) is the official specification (standard) that defines how JavaScript should work.
+/*Syntax (let, const, class)
+
+Data types
+
+Operators
+
+Control flow (if, for)
+
+Functions
+
+Objects & prototypes
+
+Modules (import, export)
+
+Promises, async/await
+
+
+It does NOT define:
+
+DOM
+
+BOM (window, document)
+
+Browser APIs
+
+Node.js APIs (fs, http)*/
+let name="ganehs"
+let age=18
+let isLoggedIn=false
+
+//primitive D.T
+//number => 2 to power 53
+//bigint
+//string=> ""
+//boolean=> true/false
+//null a standalone value also a data type
+//undefined => value not assigned
+//symbol => used in react, used to find uniqueness in components
+
+
+
+//object
+console.log(typeof("ganesh"))
+console.log(typeof(6))
+console.log(typeof(Number))
+console.log(typeof(String))
+console.log(typeof(undefined)) //undefined
+console.log(typeof(null))//object
